99670bcc3ee8235289d8c34c8ffb46f3abf3b439
Matthäus Otremba <matthaeus.otremba@procon-it.de>
2023-05-05 18:32:20 +0200

    add Step 6: Create and Edit Part 2

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -34,6 +34,39 @@ sap.ui.define([
       this._bTechnicalErrors = false;
 		},
 
+    onCreate: function() {
+      var oList = this.byId("peopleList");
+      var oBinding = oList.getBinding("");
+      var oContext = oBinding.create({
+        "UserName": "",
+        "FirstName": "",
+        "LastName": "",
+        "Age": 18
+      });
+      
+      this._setUIChanges();
+      this.getView().getModel("appView").setProperty("/usernameEmpty", true);
+
+      oList.getItems().some(function (oItem) {
+        if (oItem.getBindingContext() === oContext) {
+          oItem.focus();
+          oItem.setSelected(true);
+          return true;
+        }
+      });
+    },    
+
+    onInputChange: function(oEvt) {
+      if (oEvt.getParameter("escPressed")) {
+        this._setUIChanges();
+      } else {
+        this._setUIChanges(true);
+        if (oEvt.getSource().getParent().getBindingContext().getProperty("UserName")) {
+          this.getView().getModel("appView").setProperty("/usernameEmpty",false);
+        }
+      }
+    },
+
     onRefresh: function() {
       var oBinding = this.byId("peopleList").getBinding("items");
 
@@ -44,6 +77,30 @@ sap.ui.define([
 
       oBinding.refresh();
       MessageToast.show(this._getText("refreshSuccessMessage"));
+    },
+
+    onResetChanges: function() {
+      this.byId("peopleList").getBinding("items").resetChanges();
+      this._bTechnicalErrors = false;
+      this._setUIChanges();
+    },
+
+    onSave: function() {
+      var fnSuccess = function() {
+        this._setBusy(false);
+        MessageToast.show(this._getText("changeSentMessage"));
+        this._setUIChanges(false);
+      }.bind(this);
+
+      var fnError = function(oError) {
+        this._setBusy(false);
+        this._setUIChanges(false);
+        MessageBox.error(oError.message);
+      }.bind(this);
+
+      this._setBusy(true); // Lock UI until submitBatch is resolved.
+      this.getView().getModel().submitBatch("peopleGroup").then(fnSuccess, fnError);
+      this._bTechnicalErrors = false; // If there were technical errors, a new save resets them.
     },
 
     onSearch: function() {
@@ -67,12 +124,56 @@ sap.ui.define([
       oView.getModel("appView").setProperty("/order", iOrder);
       oView.byId("peopleList").getBinding("items").sort(sOrder && new Sorter("LastName", sOrder === "desc"));
 
-      message = this._getText("sortMessage", [this._getText(aStateTextIds[iOrder])]);
+      sMessage = this._getText("sortMessage", [this._getText(aStateTextIds[iOrder])]);
       MessageToast.show(sMessage);
+    },
+
+    onMessageBindingChange: function(oEvent) {
+      var aContexts = oEvent.getSource().getContexts(),
+          aMessages,
+          bMessageOpen = false;
+      
+      if (bMessageOpen || !aContexts.length) {
+        return;
+      }
+
+      // Extract and remove the technical messages
+      aMessages = aContexts.map(function(oContext) {
+        return oContext.getObject();
+      });
+      sap.ui.getCore().getMessageManager().removeMessages(aMessages);
+
+      this._setUIChanges(true);
+      this._bTechnicalErrors = true;
+      MessageBox.error(aMessages[0].message, {
+        id: "serviceErrorMessageBox",
+        onClose: function() {
+          bMessageOpen = false;
+        }
+      });
+
+      bMessageOpen = true;
     },
 
     _getText: function(sTextId, aArgs) {
       return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId, aArgs);
+    },
+
+    _setUIChanges: function(bHasUIChanges) {
+      if (this._bTechnicalErrors) {
+        // If there is currently a technical error, then force 'true'.
+        bHasUIChanges = true;
+      } else if (bHasUIChanges === undefined) {
+        bHasUIChanges = this.getView().getModel().hasPendingChanges();
+      }
+
+      var oModel = this.getView().getModel("appView");
+      oModel.setProperty("/hasUIChanges", bHasUIChanges);
+    },
+
+    _setBusy: function(bIsBusy) {
+      var oModel = this.getView().getModel("appView");
+      oModel.setProperty("/busy", bIsBusy);
     }
 	});
 });
